de333e8933ab0202953feb448315d3973eff3ed2
arya <aryakhanna@hotmail.com>
2020-09-10 01:53:38 +1000

    file structure init

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
-
 const port = 8000;
+
+// routes
+require('./routes')(app, {});
+
 app.listen(port, () => {  console.log('Live on ' + port); });
